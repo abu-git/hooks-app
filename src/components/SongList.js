71a1151f7a4a3ee55bfa3e022ace7473cd0f9cec
abu-git <abu.sammie@gmail.com>
2020-08-16 00:16:34 +0200

    react hooks useState

---
+++ src/components/SongList.js
@@ -0,0 +1,27 @@
+import React, { useState } from  'react';
+import { v4 } from 'uuid';
+import NewSongForm from './NewSongForm';
+
+const SongList = () => {
+	const [songs, setSongs] = useState([
+		{ title: 'almost home', id: 1 },
+		{ title: 'this wild darkness', id: 2 },
+		{ title: 'memory gospel', id: 3 }
+	]);
+
+	const addSong = (title) => {
+		setSongs([...songs, { title: title, id: v4() }]);
+	}
+	return(
+		<div className="song-list">
+			<ul>
+				{songs.map(song => {
+					return(<li key={song.id}>{song.title}</li>);
+				})}
+			</ul>
+			<NewSongForm addSong={addSong}/>
+		</div>
+	);
+}
+
+export default SongList;

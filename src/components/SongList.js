bb65f955cd6074022cabebfc8b5ac060b4b12c1a
abu-git <abu.sammie@gmail.com>
2020-08-16 13:07:55 +0200

    useEffect hook lessons

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,4 +1,4 @@
-import React, { useState } from  'react';
+import React, { useState, useEffect } from  'react';
 import { v4 } from 'uuid';
 import NewSongForm from './NewSongForm';
 
@@ -9,9 +9,19 @@ const SongList = () => {
 		{ title: 'memory gospel', id: 3 }
 	]);
 
+	const [age, setAge] = useState(20);
+
 	const addSong = (title) => {
 		setSongs([...songs, { title: title, id: v4() }]);
 	}
+
+	useEffect(() => {
+		console.log('useEffect hook ran', songs);
+	}, [songs])
+
+	useEffect(() => {
+		console.log('useEffect hook ran', age);
+	}, [age])
 	return(
 		<div className="song-list">
 			<ul>
@@ -20,6 +30,7 @@ const SongList = () => {
 				})}
 			</ul>
 			<NewSongForm addSong={addSong}/>
+			<button onClick={() => setAge(age + 1)}>Add 1 to age: {age}</button>
 		</div>
 	);
 }
